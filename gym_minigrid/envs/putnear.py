914be7b809259aeab8d0fced1e5b3f0430367f77
Kanaad Parvate <kanaadp@gmail.com>
2020-03-24 15:56:43 -0700

    passing tests

--- gym_minigrid/envs/putnear.py
+++ gym_minigrid/envs/putnear.py
@@ -93,7 +93,7 @@ class PutNearEnv(MiniGridEnv):
 
         obs, reward, done, info = super().step(action)
 
-        u, v = self.dir_vec
+        u, v = self.dir_vec()
         ox, oy = (self.agents[self.DEFAULT_AGENT_ID].pos[0] + u, self.agents[self.DEFAULT_AGENT_ID].pos[1] + v)
         tx, ty = self.target_pos
 
